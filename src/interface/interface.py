af69373d82293f08d7205f0c05f2cdc8b2effae9
Markus Kongstein Rognlien <markukro@stud.ntnu.no>
2019-09-01 17:28:18 +0200

    Add push-buttons

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -16,8 +16,9 @@ class FPCA(qtw.QWidget):
         self.maingrid = qtw.QGridLayout(self)
 
         # Creates 100 sliders
-        loose_sliders = qtw.QWidget()
-        box_of_sliders = qtw.QVBoxLayout(loose_sliders)
+        self.slider_grid = qtw.QWidget()
+        slider_grid_layout = qtw.QGridLayout(self.slider_grid)
+        slider_grid_layout.setVerticalSpacing(30)
 
         self.slider_list = []
         for i in range(N_PCA):
@@ -28,13 +29,14 @@ class FPCA(qtw.QWidget):
             slider.setMaximum(300)
             slider.setValue(0)
             self.slider_list.append(slider)
-            box_of_sliders.addWidget(slider)
+            slider_grid_layout.addWidget(qtw.QLabel(f"PC{i + 1}"), i, 0)
+            slider_grid_layout.addWidget(slider, i, 1)
 
         # Places these sliders in a scrollable area
         sliders = qtw.QScrollArea()
         sliders.setVerticalScrollBarPolicy(qtc.Qt.ScrollBarAlwaysOn)
         sliders.setHorizontalScrollBarPolicy(qtc.Qt.ScrollBarAlwaysOff)
-        sliders.setWidget(loose_sliders)
+        sliders.setWidget(self.slider_grid)
 
         # Creates a pixmap of the mean face
         self.imgarr = self.get_mean_face()
@@ -45,9 +47,19 @@ class FPCA(qtw.QWidget):
         self.face_label = qtw.QLabel()
         self.face_label.setPixmap(face_pixmap)
 
+        # Creates a grid containing three push-buttons
+        self.buttons_grid = qtw.QWidget()
+        buttons_grid_layout = qtw.QGridLayout(self.buttons_grid)
+        self.randomize_button = qtw.QPushButton("Random")
+        self.mean_button = qtw.QPushButton("Mean")
+        self.invert_button = qtw.QPushButton("Invert")
+        for i, b in enumerate([self.randomize_button, self.mean_button, self.invert_button]):
+            buttons_grid_layout.addWidget(b, 0, i)
+
         # Adds the separate widgets to the main grid
         self.maingrid.addWidget(sliders, 0, 0)
         self.maingrid.addWidget(self.face_label, 0, 1)
+        self.maingrid.addWidget(self.buttons_grid, 1, 0)
 
     def recolor(self, i):
         print(self.slider_list[i].value())

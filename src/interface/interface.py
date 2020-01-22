90fa7fc7beba9ec72c02634c85e69c1d0a8ba8ac
marod <marod@stud.ntnu.no>
2020-01-22 01:43:52 +0100

    Request faces from server

--- src/interface/interface.py
+++ src/interface/interface.py
@@ -4,13 +4,16 @@ import numpy as np
 import PySide2.QtWidgets as qtw
 import PySide2.QtCore as qtc
 import PySide2.QtGui as qtg
+import matplotlib.pyplot as plt
+
+from src.model.model_serve import ModelServer
+from pathlib import Path
 
 N_PCA = 100
 
 
 class FPCA(qtw.QWidget):
-
-    def __init__(self, parent = None):
+    def __init__(self, parent=None):
         super(FPCA, self).__init__(parent)
         self.setWindowTitle("FPCA")
         self.maingrid = qtw.QGridLayout(self)
@@ -61,11 +64,31 @@ class FPCA(qtw.QWidget):
         self.maingrid.addWidget(self.face_label, 0, 1)
         self.maingrid.addWidget(self.buttons_grid, 1, 0)
 
-    def recolor(self, i):
-        print(self.slider_list[i].value())
-        self.imgarr = np.full(self.imgarr.shape, int((self.slider_list[0].value() + 300)*255/600), dtype=np.uint8)
+        self.model_server = ModelServer(Path(__file__).parents[1] / 'model' / 'saved_models' / 'model004.h5', 'famousbirthdays')
+
+        def randomize_action():
+            self.model_server.random_person()
+            for i, pc in enumerate(self.model_server.pc_coeffs[0]):
+                self.slider_list[i].setValue(int(pc * 150))
+            self.recolor()
+
+        self.randomize_button.clicked.connect(randomize_action)
+
+    def recolor(self, i=None):
+        if i is not None:
+            self.model_server.set_pc_coeff(i, self.slider_list[i].value() / 150)
+        # self.imgarr = np.full(self.imgarr.shape, int((self.slider_list[0].value() + 300)*255/600), dtype=np.uint8)
+
+        self.model_server.update_image()
+        self.imgarr = self.model_server.image.numpy()
+
         pic = cv2.resize(self.imgarr, (0, 0), fx=10, fy=10)
-        face_image = qtg.QImage(pic.data, 640, 640, 3 * 640, qtg.QImage.Format_RGB888).rgbSwapped()
+        print(pic.shape)
+        print(pic.nbytes / pic.shape[1])
+        plt.imshow(np.array(cv2.cvtColor(pic, cv2.COLOR_BGR2RGB)))
+        pic = cv2.cvtColor(pic, cv2.COLOR_BGR2RGB)
+        plt.show()
+        face_image = qtg.QImage(pic.data, pic.shape[1], pic.shape[0], pic.shape[1]*3, qtg.QImage.Format_RGBX64).rgbSwapped()
         face_pixmap = qtg.QPixmap.fromImage(face_image)
         self.face_label.setPixmap(face_pixmap)
         self.face_label.update()
